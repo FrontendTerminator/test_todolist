42667139239e6505940ab2f700b663ee74b14c75
FrontendTerminator <mikutishvili.koba@gmail.com>
2021-02-16 16:56:27 +0300

    1. HOC React.memo and useCallback are added

--- src/AppWithRedux.tsx
+++ src/AppWithRedux.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import './App.css';
 import {TaskType, Todolist} from './Todolist';
 import {AddItemForm} from './AddItemForm';
@@ -27,63 +27,45 @@ export type TasksStateType = {
 
 
 function AppWithRedux() {
-    function removeTask(id: string, todolistId: string) {
-        dispatch(removeTaskAC(id, todolistId));
-    }
-
-    function addTask(title: string, todolistId: string) {
-        dispatch(addTaskAC(title, todolistId));
-    }
-
-    function changeFilter(value: FilterValuesType, todolistId: string,) {
-        dispatch(changeTodolistFilterAC(todolistId, value))
-    }
-
-    function changeStatus(id: string, isDone: boolean, todolistId: string) {
-        dispatch(changeTaskStatusAC(id, isDone, todolistId))
-    }
-
-    function changeTaskTitle(id: string, newTitle: string, todolistId: string) {
-        dispatch(changeTaskTitleAC(id, newTitle, todolistId));
-    }
-
-    function removeTodolist(id: string) {
-        const action = removeTodolistAC(id)
-        dispatch(action)
-    }
-
-    function changeTodolistTitle(id: string, title: string) {
-        const action = changeTodolistTitleAC(id, title)
-        dispatch(action)
-    }
-
-    function addTodolist(title: string) {
-        const action = addTodolistAC(title)
-        dispatch(action)
-    }
-
-    // let todolistId1 = v1();
-    // let todolistId2 = v1();
-
-    // let [todolists, dispatchToTodolist] = useReducer(todoListsReducer,[
-    //     {id: todolistId1, title: "What to learn", filter: "all"},
-    //     {id: todolistId2, title: "What to buy", filter: "all"}
-    // ])
-    //
-    // let [tasks, dispatchToTasks] = useReducer(tasksReducer, {
-    //     [todolistId1]: [
-    //         {id: v1(), title: "HTML&CSS", isDone: true},
-    //         {id: v1(), title: "JS", isDone: true}
-    //     ],
-    //     [todolistId2]: [
-    //         {id: v1(), title: "Milk", isDone: true},
-    //         {id: v1(), title: "React Book", isDone: true}
-    //     ]
-    // });
-
+    console.log('App')
     const todolists = useSelector<AppRootStateType, Array<TodolistType>>(state => state.todolists)
     const tasks = useSelector<AppRootStateType, TasksStateType>(state => state.tasks)
     const dispatch = useDispatch()
+
+    const removeTask = useCallback((id: string, todolistId: string) => {
+        dispatch(removeTaskAC(id, todolistId));
+    }, [dispatch])
+
+    const addTask = useCallback((title: string, todolistId: string) => {
+        dispatch(addTaskAC(title, todolistId));
+    }, [dispatch])
+
+    const changeFilter = useCallback((value: FilterValuesType, todolistId: string,) => {
+        dispatch(changeTodolistFilterAC(todolistId, value))
+    }, [dispatch])
+
+    const changeStatus = useCallback((id: string, isDone: boolean, todolistId: string)=> {
+        dispatch(changeTaskStatusAC(id, isDone, todolistId))
+    }, [dispatch])
+
+    const changeTaskTitle = useCallback((id: string, newTitle: string, todolistId: string) => {
+        dispatch(changeTaskTitleAC(id, newTitle, todolistId));
+    }, [dispatch])
+
+    const removeTodolist = useCallback((id: string) => {
+        const action = removeTodolistAC(id)
+        dispatch(action)
+    }, [dispatch])
+
+    const changeTodolistTitle = useCallback((id: string, title: string) => {
+        const action = changeTodolistTitleAC(id, title)
+        dispatch(action)
+    },[dispatch])
+
+    const addTodolist = useCallback((title: string) =>{
+        const action = addTodolistAC(title)
+        dispatch(action)
+    }, [dispatch])
 
     return (
         <div className="App">
@@ -107,13 +89,6 @@ function AppWithRedux() {
                         todolists.map(tl => {
                             let allTodolistTasks = tasks[tl.id];
                             let tasksForTodolist = allTodolistTasks;
-
-                            if (tl.filter === "active") {
-                                tasksForTodolist = allTodolistTasks.filter(t => t.isDone === false);
-                            }
-                            if (tl.filter === "completed") {
-                                tasksForTodolist = allTodolistTasks.filter(t => t.isDone === true);
-                            }
 
                             return <Grid item>
                                 <Paper style={{padding: "10px"}}>
--- src/state/tasks-reducer.test.ts
+++ src/state/tasks-reducer.test.ts
@@ -9,25 +9,10 @@ import {TasksStateType} from "../App";
 import {addTodolistAC, removeTodolistAC} from "./todolists-reducer";
 import {TaskType} from "../Todolist";
 
-// const startState: TasksStateType = {}
-//
-// beforeEach(()=>{
-//     startState = {
-//         "todolistId1": [
-//             {id: '1', title: "HTML&CSS", isDone: false},
-//             {id: '2', title: "HTML&CSS", isDone: true},
-//             {id: '3', title: "JS", isDone: false}
-//         ],
-//         "todolistId2": [
-//             {id: '1', title: "Bread", isDone: false},
-//             {id: '2', title: "Milk", isDone: true},
-//             {id: '3', title: "React Book", isDone: false}
-//         ]
-//     }
-// })
+let startState: TasksStateType
 
-test('correct task should be deleted from correct array', () => {
-    const startState = {
+beforeEach(()=>{
+    startState = {
         "todolistId1": [
             {id: '1', title: "HTML&CSS", isDone: false},
             {id: '2', title: "HTML&CSS", isDone: true},
@@ -39,6 +24,9 @@ test('correct task should be deleted from correct array', () => {
             {id: '3', title: "React Book", isDone: false}
         ]
     }
+})
+
+test('correct task should be deleted from correct array', () => {
 
     const action = removeTaskAC('2', "todolistId2")
 
@@ -50,18 +38,6 @@ test('correct task should be deleted from correct array', () => {
 })
 
 test('correct task should be added from correct array', () => {
-    const startState = {
-        "todolistId1": [
-            {id: '1', title: "HTML&CSS", isDone: false},
-            {id: '2', title: "HTML&CSS", isDone: true},
-            {id: '3', title: "JS", isDone: false}
-        ],
-        "todolistId2": [
-            {id: '1', title: "Bread", isDone: false},
-            {id: '2', title: "Milk", isDone: true},
-            {id: '3', title: "React Book", isDone: false}
-        ]
-    }
 
     const action = addTaskAC('juice', "todolistId2")
 
@@ -75,18 +51,6 @@ test('correct task should be added from correct array', () => {
 })
 
 test('status of specified task should be changed', () => {
-    const startState: TasksStateType = {
-        "todolistId1": [
-            {id: "1", title: "CSS", isDone: false},
-            {id: "2", title: "JS", isDone: true},
-            {id: "3", title: "React", isDone: false}
-        ],
-        "todolistId2": [
-            {id: "1", title: "bread", isDone: false},
-            {id: "2", title: "milk", isDone: true},
-            {id: "3", title: "tea", isDone: false}
-        ]
-    };
 
     const action = changeTaskStatusAC("2", false, "todolistId2");
 
@@ -97,18 +61,6 @@ test('status of specified task should be changed', () => {
 });
 
 test('task title should be changed', () => {
-    const startState: TasksStateType = {
-        "todolistId1": [
-            {id: "1", title: "CSS", isDone: false},
-            {id: "2", title: "JS", isDone: true},
-            {id: "3", title: "React", isDone: false}
-        ],
-        "todolistId2": [
-            {id: "1", title: "bread", isDone: false},
-            {id: "2", title: "milk", isDone: true},
-            {id: "3", title: "tea", isDone: false}
-        ]
-    };
 
     const action = changeTaskTitleAC("2", "milky way", "todolistId2");
 
@@ -119,18 +71,6 @@ test('task title should be changed', () => {
 });
 
 test('new array should be added when new todolist is added', () => {
-    const startState: TasksStateType = {
-        "todolistId1": [
-            {id: "1", title: "CSS", isDone: false},
-            {id: "2", title: "JS", isDone: true},
-            {id: "3", title: "React", isDone: false}
-        ],
-        "todolistId2": [
-            {id: "1", title: "bread", isDone: false},
-            {id: "2", title: "milk", isDone: true},
-            {id: "3", title: "tea", isDone: false}
-        ]
-    };
 
     const action = addTodolistAC("new todolist");
 
@@ -148,18 +88,6 @@ test('new array should be added when new todolist is added', () => {
 });
 
 test('property with todolistId should be deleted', () => {
-    const startState: TasksStateType = {
-        "todolistId1": [
-            { id: "1", title: "CSS", isDone: false },
-            { id: "2", title: "JS", isDone: true },
-            { id: "3", title: "React", isDone: false }
-        ],
-        "todolistId2": [
-            { id: "1", title: "bread", isDone: false },
-            { id: "2", title: "milk", isDone: true },
-            { id: "3", title: "tea", isDone: false }
-        ]
-    };
 
     const action = removeTodolistAC("todolistId2");
 
